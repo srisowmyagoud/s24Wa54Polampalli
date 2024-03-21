09560f68475dfcec85057086fb4d1d57b9c863e9
srisowmyagoud <polampallisowmya@gmail.com>
2024-03-20 19:30:09 -0500

    c2

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var factor = 1;
-var product = 1;
-/* GET users listing. */
+
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  factor++;
-  product = product*factor;
-  res.send(`Product is${product}`);
+  res.render('mydata', { title: 'Sowmya polampalli' });
 });
 
 module.exports = router;
